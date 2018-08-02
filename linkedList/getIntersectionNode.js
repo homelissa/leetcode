a74e6f6a283d02f4bddba0c2a5baa4dcc70ae493
Melissa Ho <homelissa93@gmail.com>
2018-08-02 08:31:38 -0700

    getIntersectionNode

---
+++ linkedList/getIntersectionNode.js
@@ -0,0 +1,14 @@
+var getIntersectionNode = function(headA, headB) {
+    if (!headA || !headB) return null;
+    var p1 = headA;
+    var p2 = headB;
+    while (p1 && p2 && p1 !== p2) {
+        p1 = p1.next;
+        p2 = p2.next;
+
+        if (p1 == p2) return p1;
+        if (!p1) p1 = headB;
+        if (!p2) p2 = headA;
+    }
+    return p1;
+};
