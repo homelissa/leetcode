855d59e49b4d892e025a741c6ee8660755e8a9c0
Melissa Ho <homelissa93@gmail.com>
2018-08-09 09:17:44 -0700

    power of three

---
+++ powerOfThree.js
@@ -0,0 +1,18 @@
+var isPowerOfThree = function(n) {
+    if (n === 0) {
+        return false;
+    }
+
+    if (n === 1 || n === 3) {
+        return true;
+    }
+
+    let div = n / 3;
+
+    if (Number.isInteger(div)) {
+        return isPowerOfThree(div);
+    }
+
+    return false;
+
+};
