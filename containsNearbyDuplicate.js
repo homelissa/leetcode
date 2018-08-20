b5a2c12a7d7faa8ba18d4bfe55eec18ba5ae40ab
Melissa Ho <homelissa93@gmail.com>
2018-08-20 09:15:45 -0700

    contains nearby duplicate

---
+++ containsNearbyDuplicate.js
@@ -0,0 +1,30 @@
+/*
+Given an array of integers and an integer k, find out whether there are two distinct indices i and j in the array such that nums[i] = nums[j] and the absolute difference between i and j is at most k.
+
+Example 1:
+
+Input: nums = [1,2,3,1], k = 3
+Output: true
+Example 2:
+
+Input: nums = [1,0,1,1], k = 1
+Output: true
+Example 3:
+
+Input: nums = [1,2,3,1,2,3], k = 2
+Output: false
+*/
+
+var containsNearbyDuplicate = function(nums, k) {
+  let hashMap = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if ((i - hashMap[nums[i]]) <= k) {
+      return true;
+    }
+
+    hashMap[nums[i]] = i;
+  }
+
+  return false;
+}
